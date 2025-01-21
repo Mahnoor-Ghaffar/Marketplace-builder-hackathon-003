fca8fa25ca452ada2737af56db62f87064488795
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-01-22 03:57:09 +0500

    add
    add

--- src/app/components/ContactComponents/LetsTalk.tsx
+++ src/app/components/ContactComponents/LetsTalk.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import React from 'react'
-import arrow from "@/../public/assets/Homepage/Arrow 2.png";
+import Image from 'next/image';
+import React from 'react';
+import arrow from '@/../public/assets/Homepage/Arrow 2.png';
 
 const LetsTalk = () => {
   return (
@@ -20,7 +20,7 @@ const LetsTalk = () => {
             </h5>
             {/* Main Title */}
             <h1 className="font-Montserrat font-bold text-[40px] lg:text-[58px] leading-[48px] lg:leading-[80px] tracking-[0.2px] text-[#252B42]">
-              Let’s Talk
+              Let&#39;s Talk
             </h1>
             {/* Button */}
             <button className="rounded-[5px] py-[12px] px-[20px] lg:py-[15px] lg:px-[40px] bg-[#23A6F0] text-white font-Montserrat font-bold text-[14px] leading-[22px]">
@@ -29,9 +29,8 @@ const LetsTalk = () => {
           </div>
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default LetsTalk
+export default LetsTalk;
